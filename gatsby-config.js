a4df622b8c71602d7fc6083137ca6a575e8ecbb2
Daniel Ngo <daniel.ngo@monash.edu>
2022-08-08 11:28:23 +1000

    chore: initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,11 +23,16 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `b42jfuaf`,
+        dataset: `production`,
+        token: process.env.SANITY_TOKEN,
       },
     },
   ],
---
+++ src/dashboardConfig.js
@@ -0,0 +1,14 @@
+export default {
+  widgets: [
+    {
+      name: 'gatsby',
+      options: {
+        sites: [
+          {
+            siteUrl: 'intrinsicdg.gtsb.io',
+          },
+        ],
+      },
+    },
+  ],
+}
